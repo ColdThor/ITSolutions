b5189410c0dacc8e6c56dd3a4f88e763e2ee28cd
JohnnySima <sima.johnny@gmail.com>
2018-12-03 22:49:38 +0100

    grafické doladenie sekcie "moje inzeráty" + malé úpravy na search result

--- resources/views/frontend/search_results.blade.php
+++ resources/views/frontend/search_results.blade.php
@@ -14,29 +14,28 @@
                 foreach($search as $ads):
                 $fotka = $ads->id_advertisement;
                 $i++; ?>
-                <div class="col-lg-6">
-                    <div class="single-property">
-                        <div class="images">
-                            <a href="<?php echo url('/inzerat/'.$fotka.'/false'); ?>" style="color: black">    <img class="img-fluid mx-auto d-block"
-                                                                                                                    src="/reality/storage/app/public/inzeraty/inzerat_<?php echo $fotka ?>/fotka_1.png" alt="" style="max-height: 250px"> </a>
-                            <span>Typ predaja: {{$ads->type}} </span>
+            <div class="col-lg-6">
+                <div class="single-property">
+                    <div class="images">
+                        <a href="<?php echo url('/inzerat/'.$fotka.'/false'); ?>" style="color: black">
+                            <img class="img-fluid mx-auto d-block" src="/reality/storage/app/public/inzeraty/inzerat_<?php echo $fotka ?>/fotka_1.png" alt=""> </a>
+                        <span>{{$ads->type}} </span>
+                    </div>
+                    <div class="desc">
+                        <div class="top d-flex justify-content-between">
+                            <h5><a href="<?php echo url('/inzerat/'.$fotka.'/false'); ?>"> {{$ads->title}}</a></h5>
                         </div>
-
-                        <div class="desc">
-                            <div class="top d-flex justify-content-between">
-                                <h5>  <a href="<?php echo url('/inzerat/'.$fotka.'/false'); ?>"> {{$ads->title}}</a></h5>
-                            </div>
-                            <div class="bottom d-flex justify-content-start">
-                                <p><span class="lnr lnr-apartment"></span>Cena: <b>{{$ads->price}}€</b></p>
-                                <p><span class="lnr lnr-crop"></span>Výmera: <b>{{$ads->area}}m<sup>2</sup></b></p>
-                            </div>
-                            <div class="bottom d-flex justify-content-start">
-                                <p><span class="lnr lnr-map-marker"></span>Poloha: {{$ads->location}}</p>
-                                <p><span class="lnr lnr-history"></span>Pridané: <b>{{$ads->date}}</b></p>
-                            </div>
+                        <div class="bottom d-flex justify-content-start">
+                            <p><span class="lnr lnr-apartment"></span>Cena: <b>{{$ads->price}}€</b></p>
+                            <p><span class="lnr lnr-crop"></span>Výmera: <b>{{$ads->area}}m<sup>2</sup></b></p>
+                        </div>
+                        <div class="bottom d-flex justify-content-start">
+                            <p><span class="lnr lnr-map-marker"></span>Poloha: {{$ads->location}}</p>
+                            <p><span class="lnr lnr-history"></span>Pridané: <b>{{$ads->date}}</b></p>
                         </div>
                     </div>
                 </div>
+            </div>
                 <?php endforeach;
 
                 if($i==0): ?>
