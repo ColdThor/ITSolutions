57400c9c16226d76b03df832303e9589f623054b
JohnnySima <sima.johnny@gmail.com>
2018-12-07 21:40:27 +0100

    doladenie stránky kúpa

--- resources/views/frontend/kupa.blade.php
+++ resources/views/frontend/kupa.blade.php
@@ -1,32 +1,57 @@
 @include('frontend/header')
 @include('frontend/chat')
 @section('page') Kúpa, predaj, nájom @endsection
-<br/><br/><br/>
-<h2 style="margin: 2%">Stiahnite si dokument, ktorý potrebujete v pdf formáte</h2>
-
-<h3 style="margin: 2%" ><a href="/reality/assets/img/ziadost-o-predlzenie-najomnej-zmluvy.pdf" download> Žiadosť o predĺženie nájomnej zmluvy</a></h3>
-<p style="margin: 2%">Vzor žiadosti slúži na predĺženie nájomnej zmluvy zo strany nájomcu pre prenajímateľa, ak v nájomnej zmluve bol nájom na dobu určitú. Uvedú sa tu informácie o prenajímateľovi aj nájomcovi, opíše sa predmetný byt, výška nájomného, dátum podpísania nájomnej zmluvy. Spomenie sa dátum ukončenia nájmu podľa nájomnej zmluvy a dátum, do ktorého chce nájomca predĺžiť nájomnú zmluvu.</p>
-
-<h3 style="margin: 2%"><a href="/reality/assets/img/ziadost-o-pridelenie-najomneho-bytu.pdf" download> Žiadosť o pridelenie nájomného bytu</a></h3>
-<p style="margin: 2%">Vzor žiadosti slúži na pridelenie nájomného bytu z dôvodu majetkových pomerov jeho rodiny a detí. K žiadosti sa prikladá: čestné vyhlásenia o majetkových pomeroch rodiny, potvrdenia zamestnávateľa o pracovnom pomere, doklad o výške mesačných príjmov všetkých osôb uvedených v žiadosti (okrem nezaopatrených detí) za predchádzajúci kalendárny rok. Žiadosť sa podáva na príslušný Obvodný úrad.</p>
-
-<h3 style="margin: 2%"><a href="/reality/assets/img/ziadost-o-vydanie-rovnopisu-z-notarskej-zapisnice.pdf" download> Žiadosť o vydanie rovnopisu z notárkej zápisnice</a></h3>
-<p style="margin: 2%">Vzor žiadosti slúži na požiadanie notára o vydanie rovnopisu z notárskej zápisnice pri predaji nehnuteľnosti. Uvedie sa, medzi kým bola predmetná notárska zápisnica spísaná a čoho sa týkala. Žiada sa aj informácia, kedy si bude môcť žiadateľ rovnopis prevziať.</p>
-
-<h3 style="margin: 2%"><a href="/reality/assets/img/zmluva-o-zriadeni-spolocenstva-vlastnikov-bytov-a-nebytovych-priestorov.pdf" download> Zmluva o zriadení spoločenstva vlastníkov bytov a nebytových priestorov</a></h3>
-<p style="margin: 2%">Vzor zmluvy slúži na určenie orgánov, názvu, sídla, orgánov spoločenstva, jeho predsedu, rady, informácie o zhromaždeniach, spôsob správy spoločných častí domu, spoločných zariadení domu, spoločných nebytových priestorov, príslušenstva a pozemku, úpravu práv a povinností vlastníkov bytov a nebytových priestorov v dome, úpravu majetkových  pomerov spoločenstva, spôsob a oprávnenie hospodárenia s prostriedkami fondu prevádzky, údržby a opráv, okolnosti jeho zrušenia.</p>
-
-<h3 style="margin: 2%"><a href="/reality/assets/img/zmluva-o-zruseni-a-vysporiadani-podieloveho-spoluvlastnictva-k-pozemku-a-nehnutelnosti.pdf" download>Zmluva o zrušení a vysporiadaní podielového spoluvlastníctva k pozemku a nehnuteľnosti</a></h3>
-<p style="margin: 2%">Vzor zmluvy slúži na dohodu medzi spoluvlastníkmi na zrušenie spoluvlastníctva na pozemok a nehnuteľnosti a vysporiadanie formou rozdelenia. V zmluve sa uvedú informácie o konkrétnom pozemku a nehnuteľnosti, hodnota vysporiadanej parcely, detaily o vzájomnom vysporiadaní spoluvlastníkov. Účastníci zmluvy súhlasia, aby Katastrálny úrad na základe tejto dohody zapísal vklad vlastníckeho práva.</p>
-
-
-
-<br/><br/><br/><br/><br/>
-
-
-
-
-
-
-
+<div align="center">
+    <section class="service-area section-gap">
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-8 pt-50 pb-30 header-text">
+                    <h3>Všetky potrebné dokumenty na jednom mieste</h3>
+                </div>
+            </div>
+            <div class="row pb-20" >
+                <div class="col-lg-4 col-md-6 pb-30">
+                    <a href="/reality/assets/img/ziadost-o-predlzenie-najomnej-zmluvy.pdf" download>
+                        <div class="single-service" >
+                            <h4>Vzor žiadosti o predĺženie nájomnej zmluvy</h4>
+                            <a href="/reality/assets/img/ziadost-o-predlzenie-najomnej-zmluvy.pdf" download class="genric-btn default circle">Stiahnuť</a>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 pb-30">
+                    <a href="/reality/assets/img/ziadost-o-pridelenie-najomneho-bytu.pdf" download="">
+                        <div class="single-service">
+                            <h4>Vzor žiadosti o pridelenie nájomného bytu</h4>
+                            <a href="/reality/assets/img/ziadost-o-pridelenie-najomneho-bytu.pdf" download class="genric-btn default circle">Stiahnuť</a>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 pb-30">
+                    <a href="/reality/assets/img/ziadost-o-vydanie-rovnopisu-z-notarskej-zapisnice.pdf" download="">
+                        <div class="single-service">
+                            <h4>Vzor žiadosti o vydanie rovnopisu z notárskej zápisnice</h4>
+                            <a href="/reality/assets/img/ziadost-o-vydanie-rovnopisu-z-notarskej-zapisnice.pdf" download class="genric-btn default circle">Stiahnuť</a>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 pb-30">
+                    <a href="/reality/assets/img/zmluva-o-zriadeni-spolocenstva-vlastnikov-bytov-a-nebytovych-priestorov.pdf" download="">
+                        <div class="single-service">
+                            <h4>Zmluva o zriadení spoločenstva vlastníkov bytov</h4>
+                            <a href="/reality/assets/img/zmluva-o-zriadeni-spolocenstva-vlastnikov-bytov-a-nebytovych-priestorov.pdf" download class="genric-btn default circle">Stiahnuť</a>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 pb-30">
+                    <a href="/reality/assets/img/zmluva-o-zruseni-a-vysporiadani-podieloveho-spoluvlastnictva-k-pozemku-a-nehnutelnosti.pdf" download="">
+                        <div class="single-service">
+                            <h4>Zmluva o zrušení a vysporiadaní podielového spoluvlastníctva</h4>
+                            <a href="/reality/assets/img/zmluva-o-zruseni-a-vysporiadani-podieloveho-spoluvlastnictva-k-pozemku-a-nehnutelnosti.pdf" download class="genric-btn default circle">Stiahnuť</a>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 @include('frontend/footer')
