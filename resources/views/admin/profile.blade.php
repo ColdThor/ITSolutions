771f9d88a1d513e392e5e63bb4a9282fd1865640
JohnnySima <sima.johnny@gmail.com>
2018-12-13 23:45:04 +0100

    admin register + profile css upravy

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -4,43 +4,49 @@
 
 <section class="content">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-8">
             <div class="box box-default">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Profil: {{$users->first_name . " ". $users->last_name}}</h3>
+                    <h3 class="box-title">Používateľ: {{$users->first_name . " ". $users->last_name}}</h3>
                 </div>
 
                 <div class="box-body">
-                <h4>Zmena používateľského hesla:</h4>
+                    <strong>
+                    <i class="fa fa-user-circle margin-r-5"></i>Zmena prihlasovacieho hesla:
+                    </strong>
+                </div>
+
+                <div class="box-body">
                 <form method="post" action="{{  action('UserController@changepass', ['id' => $users->id_user])  }}">
-                        <div class="form-group">
-                            <label for="password">Zadajte nové heslo:</label>
-                            <div class="input-group">
-                                <div class="input-group-addon">
-                                    <i class="fa fa-lock"></i>
-                                </div>
-                                <input type="password" name="password" class="form-control" id="password" placeholder="Zadajte nové heslo:">
-                            </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label for="password_confirmation">Zopakujte nové heslo:</label>
-                            <div class="input-group">
-                                <div class="input-group-addon">
-                                    <i class="fa fa-lock"></i>
-                                </div>
-                                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" placeholder="Zopakujte nové heslo:">
+                    <div class="form-group">
+                        <label for="password">Zadajte nové heslo</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="fa fa-lock"></i>
                             </div>
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Zadajte nové heslo">
                         </div>
+                    </div>
 
-                        <div class="box-footer">
-                            <button type="submit" name="submit" value="submit" class="btn btn-default">Zmeniť heslo</button>
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            <h5 class="text-red">
-                                {{ session('status') }}
-                                {{ $errors->first('password') }}
-                            </h5>
+                    <div class="form-group">
+                        <label for="password_confirmation">Zopakujte nové heslo</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="fa fa-lock"></i>
+                            </div>
+                            <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" placeholder="Zopakujte nové heslo">
                         </div>
+                    </div>
+
+                    <div class="box-footer">
+                        <button type="submit" name="submit" value="submit" class="btn btn-default">Zmeniť heslo</button>
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <h5 class="text-red">
+                            {{ session('status') }}
+                            {{ $errors->first('password') }}
+                        </h5>
+                    </div>
                 </form>
                 </div>
             </div>
