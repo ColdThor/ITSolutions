78071acd83b0d8e8389e4eb000b9655557aa9c14
ColdThor <chrisfodor333@gmail.com>
2018-12-13 21:00:46 +0100

    Live server zmeny

--- resources/views/frontend/inzerat.blade.php
+++ resources/views/frontend/inzerat.blade.php
@@ -25,7 +25,7 @@
         <div class="col-lg-7 col-md-7">
             <?php $id= $ad->id_advertisement; $i = 1;
             while(file_exists("storage/inzeraty/inzerat_".$id."/fotka_".$i.".png")):?>
-            <img  class="mySlides img-fluid mx-auto" src="/storage/app/public/inzeraty/inzerat_{{$ad->id_advertisement}}/fotka_<?php echo $i; ?>.png" alt="Foto1">
+            <img  class="mySlides img-fluid mx-auto"  src="/storage/app/public/inzeraty/inzerat_{{$ad->id_advertisement}}/fotka_<?php echo $i; ?>.png" alt="Foto1">
             <?php
             $i++;
             endwhile; ?>
