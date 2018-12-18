3cbdd8362a329e038d9582d0163fc9d96e23ff15
JohnnySima <sima.johnny@gmail.com>
2018-12-18 22:33:33 +0100

    admin - pridanie grafov

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -49,14 +49,14 @@ public function index() {
             ->addRow([$date . "   &   " . $yesterday, $data, $data2]);
 
         Lava::ColumnChart('Finances', $chart, [
-            'title' => 'Počet najnovších zaregistrovaných používateľov',
+            'title' => 'Počet najnovšie zaregistrovaných používateľov',
             'legend' => [
-                'position' => 'top'
+                'position' => 'bottom'
             ],
 
         ]);
 
-
+        //chart - počet pridaných inzerátov v časovom období
         $inzeraty = Lava::DataTable();
 
         $pocet = DB::table('advertisement')
@@ -65,18 +65,67 @@ public function index() {
             ->get();
 
         $inzeraty->addStringColumn('datum')
-            ->addNumberColumn('pocet');
+            ->addNumberColumn('počet inzerátov');
         foreach($pocet as $poc) {
             $inzeraty->addRow([$poc->date, $poc->pocet]);
                 }
             ;
 
         Lava::AreaChart('Inzeraty', $inzeraty, [
-            'title' => 'Počet pridaných inzerátov',
+            'title' => 'Počet pridaných inzerátov v čase',
             'legend' => [
                 'position' => 'bottom'
             ]
         ]);
+
+        //chart - 3 používatelia s najväčším počtom inzerátov
+        $inzerenti = Lava::DataTable();
+
+        $udaje = DB::table('advertisement')
+            ->join('user', 'advertisement.id_user', '=', 'user.id_user')
+            ->select(DB::raw('COUNT(id_advertisement) AS pocet'),'first_name', 'last_name')
+            ->groupBy('first_name')
+            ->orderByRaw('pocet DESC')
+            ->take(3)
+            ->get();
+
+        $inzerenti->addStringColumn('inzerent')
+            ->addNumberColumn('počet inzerátov');
+        foreach($udaje as $inz) {
+            $inzerenti->addRow([$inz->first_name. " " .$inz->last_name, $inz->pocet]);
+        }
+        ;
+
+        Lava::BarChart('Inzerenti', $inzerenti, [
+            'title' => 'Používatelia s najväčším počtom inzerátov',
+            'legend' => [
+                'position' => 'bottom'
+            ]
+        ]);
+
+        //chart - počet inzerátov pre dané kraje
+        $kraje = Lava::DataTable();
+
+        $datakraj = DB::table('advertisement')
+            ->join('location', 'advertisement.id_location', '=', 'location.id_location')
+            ->select(DB::raw('COUNT(id_advertisement) AS pocet'),'region')
+            ->groupBy('region')
+            ->get();
+
+        $kraje->addStringColumn('kraj')
+        ->addNumberColumn('počet inzerátov');
+        foreach($datakraj as $ud) {
+            $kraje->addRow([$ud->region, $ud->pocet]);
+        }
+        ;
+
+        Lava::DonutChart('Kraje', $kraje, [
+            'title' => 'Podiel inzerátov podľa krajov',
+            'legend' => [
+                'position' => 'bottom'
+            ]
+        ]);
+
 
         return view('admin/admin_home');
     } else {
--- resources/views/admin/admin_home.blade.php
+++ resources/views/admin/admin_home.blade.php
@@ -11,19 +11,33 @@
             <div class="col-xs-12">
                 <div class="box">
                     <div class="box-header">
-                        <h3 class="box-title">Prehľad</h3>
+                        <h3 class="box-title">Štatistika portálu</h3>
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-                            <div id="pop_div"></div>
-                            <?= Lava::render('ColumnChart', 'Finances', 'pop_div') ?>
-                    </div>
-                    <br>
-                    <br>
-                    <div class="box-body">
                             <div id="are_div"></div>
                             <?= Lava::render('AreaChart', 'Inzeraty', 'are_div') ?>
+                    </div><br><br>
+
+                    <div class="box-body">
+                        <div id="chart_div"></div>
+                        <?= Lava::render('DonutChart', 'Kraje', 'chart_div') ?>
                     </div>
+                    <br><br>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="box-body">
+                                <div id="pop_div"></div>
+                                <?= Lava::render('ColumnChart', 'Finances', 'pop_div') ?>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="box-body">
+                                <div id="poll_div"></div>
+                                <?= Lava::render('BarChart', 'Inzerenti', 'poll_div') ?>
+                            </div>
+                        </div>
+                    </div><br><br>
                     <!-- /.box-body -->
                 </div>
                 <!-- /.box -->
